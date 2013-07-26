0cddb8487e1413c3f77ecef3b8f7d9b40a3ed55f
Mehdi Sqalli <mehdisv@gmail.com>
2013-07-26 15:10:13 +0000

    test

--- web.js
+++ web.js
@@ -3,12 +3,11 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-
-
-  response.send('ok');
+  response.send('Hello World!');
 });
 
 Var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+
 });
